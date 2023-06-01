642f629df55834c1c2bc9063df7a7aa5c59128a7
Aleksandr Zinovyev <zinovyevaleks@yandex.ru>
2023-06-01 23:24:06 +0700

    add keys to list items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 function App() {
 
     const data = [
-        { name: 'Иван Иванов', salary: 800, increase: false },
-        { name: 'Пётр Петров', salary: 4000, increase: true },
-        { name: 'Семён Семёнов', salary: 5000, increase: false },
+        { name: 'Иван Иванов', salary: 800, increase: false, id: 1 },
+        { name: 'Пётр Петров', salary: 4000, increase: true, id: 2},
+        { name: 'Семён Семёнов', salary: 5000, increase: false, id: 3 },
     ];
 
     return (
