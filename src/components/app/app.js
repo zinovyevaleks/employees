5eb70e44dcfe04a4f31f8b139fd5ff4bced0f211
Aleksandr Zinovyev <zinovyevaleks@yandex.ru>
2023-06-12 17:08:21 +0700

    redo the app component from a functional to a class, add a method for removing an employee from the table

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,44 @@ import EmployersAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-
-    const data = [
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
         { name: 'Иван Иванов', salary: 800, increase: false, id: 1 },
-        { name: 'Пётр Петров', salary: 4000, increase: true, id: 2},
+        { name: 'Пётр Петров', salary: 4000, increase: true, id: 2 },
         { name: 'Семён Семёнов', salary: 5000, increase: false, id: 3 },
-    ];
+      ]
+    }
+  }
 
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+      data: data.filter(item => item.id !== id)
+      }
+    })
+  }
+
+  render() {
     return (
-        <div className="app">
-            <AppInfo />
+      <div className="app">
+        <AppInfo />
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
-            </div>
-
-            <EmployersList data={data}/>
-            <EmployersAddForm/>
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
         </div>
+
+        <EmployersList
+          data={this.state.data}
+          onDelete={this.deleteItem}
+        />
+        <EmployersAddForm />
+      </div>
     )
+  }
 }
 
 export default App;
